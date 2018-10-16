083be33ccac2c84f3d436ae262bc164d2053439b
shreya <sk2645@njit.edu>
2018-10-16 19:04:49 -0400

    Task: Created two page website

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('content')
+
+    <h1><center>About!!</center></h1>
+
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('content')
+
+    <h1><center>Welcome to HomePage!!</center></h1>
+
+@stop
